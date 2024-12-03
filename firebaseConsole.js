474d40c4d3e69b008b7e61d3daabf044b4f1b9e2
iBurnCake <75271603+iBurnCake@users.noreply.github.com>
2024-12-03 14:15:15 -0500

    Update firebaseConsole.js

--- firebaseConsole.js
+++ firebaseConsole.js
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDD1nHLFLlEZzfmPLNITGv0jtQ0wKUZ11k",
     authDomain: "lukeshelp.firebaseapp.com",
     projectId: "lukeshelp",
-    storageBucket: "lukeshelp.firebasestorage.app",
+    storageBucket: "lukeshelp.appspot.com", // Fixed storageBucket URL
     messagingSenderId: "1099033563606",
     appId: "1:1099033563606:web:f41f2bed1fd270d7119477"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { app, auth };
+// Initialize Firebase Auth and Google Provider
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+// Export Auth and Google Provider
+export { auth, googleProvider };
